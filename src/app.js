32adc51104909aaa7fc0b507e473023ea8de3f02
Zekumoru Dragonhart <dev@zekumoru.com>
2022-10-31 16:00:08 +0100

    Add dropdown animation and toggle

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'normalize.css';
 import './styles.css';
+
+const tab = document.querySelector('.tab');
+const dropdown = document.querySelector('.dropdown');
+tab.addEventListener('click', () => {
+  dropdown.classList.toggle('open');
+});
