d706cb65932ee4187687c14caf3ca678aba9a37c
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-02 15:29:22 +0100

    Add mobile menu

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     'no-new': 'off',
     'consistent-return': 'off',
     'max-len': 'off',
+    'no-continue': 'off',
   },
 };
--- src/app.js
+++ src/app.js
@@ -15,3 +15,27 @@ new Dropdown(dropdown, dropdownMenu, {
   },
   animationDuration: '300ms',
 });
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuItems = mobileMenu.querySelector('ul').children;
+
+new ResizeObserver(() => {
+  for (let i = menuItems.length - 1; mobileMenu.scrollWidth > mobileMenu.offsetWidth && i >= 0; i--) {
+    const menuItem = menuItems[i];
+    if (menuItem.classList.contains('more')) continue;
+
+    menuItem.style.display = 'none';
+  }
+
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i];
+    if (menuItem.classList.contains('more')) continue;
+    if (!menuItem.style.display) continue;
+
+    menuItem.style.display = '';
+    if (mobileMenu.scrollWidth > mobileMenu.offsetWidth) {
+      menuItem.style.display = 'none';
+      break;
+    }
+  }
+}).observe(mobileMenu);
