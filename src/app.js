1c6c046e814dd976dddfd6431afee4222eb290ab
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-03 11:59:25 +0100

    Add dropdown on mobile menu

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import 'normalize.css';
 import Dropdown from './components/Dropdown';
 import './styles.css';
 
-const dropdown = document.querySelector('.dropdown');
+const dropdown = document.querySelector('.dropdown-1');
 const dropdownText = dropdown.querySelector('.text');
 const dropdownMenu = dropdown.querySelector('.dropdown-menu');
 
@@ -20,7 +20,34 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuItems = mobileMenu.querySelector('ul').children;
 const moreItem = mobileMenu.querySelector('li.more');
 
+const onResize = (hiddenItems) => {
+  // Create dropdown if doesn't exist
+  let dropdownMenu = moreItem.querySelector('.dropdown-menu');
+
+  if (!dropdownMenu) {
+    dropdownMenu = document.createElement('ul');
+    dropdownMenu.className = 'dropdown-menu dropdown-menu-animated dropdown-menu-2';
+    new Dropdown(moreItem, dropdownMenu, {
+      open: 'open',
+      animationDuration: '300ms',
+    });
+
+    moreItem.appendChild(dropdownMenu);
+  }
+
+  // Populate dropdown items
+  dropdownMenu.innerHTML = '';
+
+  hiddenItems.forEach((hiddenItem) => {
+    const dropdownItem = document.createElement('li');
+    dropdownItem.textContent = hiddenItem.textContent;
+    dropdownMenu.appendChild(dropdownItem);
+  });
+};
+
 new ResizeObserver(() => {
+  const hiddenItems = [];
+
   let width = 0;
   let indexWithMore = -1;
   moreItem.style.display = '';
@@ -41,6 +68,7 @@ new ResizeObserver(() => {
   // If all list items can fit then do not render 'More'
   if (width <= mobileMenu.offsetWidth) {
     moreItem.style.display = 'none';
+    if (typeof onResize === 'function') onResize(hiddenItems);
     return;
   }
 
@@ -54,5 +82,8 @@ new ResizeObserver(() => {
     const menuItem = menuItems[i];
     if (menuItem === moreItem) continue;
     menuItem.style.display = 'none';
+    hiddenItems.push(menuItem);
   }
+
+  if (typeof onResize === 'function') onResize(hiddenItems);
 }).observe(mobileMenu);
